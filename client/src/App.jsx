7c6d5f1dec1c13566ee82eaa61037543d7984411
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-10-12 14:02:17 +0300

    styling the data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
         <h1 className="text-5xl font-bold text-center py-3 font-serif">Feature Products</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center items-center px-5">
         {data.products.map(product =>(
-          <div key={product.slug} className="flex flex-col justify-center items-center">
+          <div key={product.slug} className="flex flex-col justify-center items-center border-2 border-gray-600 p-3 rounded-2xl">
             <img src={product.image} alt={product.name} className="h-[500px] w-96 rounded pb-10"/>
             <h1 className="text-center font-bold text-lg font-serif">{product.name}</h1>
             <p className="text-center font-bold text-lg font-serif">${product.price}</p>
