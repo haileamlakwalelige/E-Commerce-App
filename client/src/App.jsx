87c7bdb7a1a6737830aa0c1b01f08d3e36edf37b
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-10-12 13:28:36 +0300

    displaying the data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import data from "./data";
+
 export default function App() {
   return (
     <div>
       <header className='bg-gray-700 text-white font-semibold text-lg py-3 px-20'>
         Amazona
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Feature Products</h1>
+        {data.products.map(product =>(
+          <div key={product.slug}>
+            <img src={product.img} alt={product.name} />
+            <h1>{product.name}</h1>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
