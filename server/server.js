6008ec17f60f2d55808b5430f309b97af33e5424
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-10-12 16:03:44 +0300

    set up and run the backend api

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
-import express from 'express'
+// import express from 'express';
+// import data from './data.js';
+const express = require('express');
+const data = require('./data.js');
+const dotenv= require('dotenv').config();
 
 const app = express();
 
 app.get('/api/products', (req, res)=>{
-    
+    res.send(data.products)
 })
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, ()=>{
+    console.log(`server is running in port ${PORT}`)
+});
