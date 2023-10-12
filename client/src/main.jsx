e981d9999feb369f95598daaa8f201bc4f5efdc1
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-10-12 23:03:01 +0300

    adding bootstrap

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
