d48848b9731e95223ffc23722aec2b2605d5cfd4
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-10-12 13:48:45 +0300

    styling the data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,21 @@ import data from "./data";
 
 export default function App() {
   return (
-    <div>
-      <header className='bg-gray-700 text-white font-semibold text-lg py-3 px-20'>
+    <div >
+      <header className='bg-gray-700 text-white font-semibold text-lg py-3 px-20 sticky top-0'>
         Amazona
       </header>
-      <main>
-        <h1>Feature Products</h1>
+      <main className="pb-20 pt-10">
+        <h1 className="text-5xl font-bold text-center py-3 font-serif">Feature Products</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center items-center px-5">
         {data.products.map(product =>(
-          <div key={product.slug}>
-            <img src={product.img} alt={product.name} />
-            <h1>{product.name}</h1>
-            <p>{product.price}</p>
+          <div key={product.slug} className="flex flex-col justify-center items-center">
+            <img src={product.image} alt={product.name} className="h-[500px] w-96 rounded pb-10"/>
+            <h1 className="text-center font-bold text-lg font-serif">{product.name}</h1>
+            <p className="text-center font-bold text-lg font-serif">${product.price}</p>
           </div>
         ))}
+        </div>
       </main>
     </div>
   );
--- client/src/data.js
+++ client/src/data.js
@@ -1,5 +1,6 @@
 // import p1 from './images/p1.jpg';
 // import p2 from './images/p2.jpg';
+// import p3 from './images/p3.jpg';
 
 
 const data = {
