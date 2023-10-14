228e7c05f515b0ce2c4af13e0a5e6d3e827e5f05
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-10-14 20:20:30 +0300

    hide the underline to none

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -4,8 +4,8 @@ import { Outlet, Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div>
-      <Link to='/'>
-        <header className='bg-gray-700 text-white font-semibold text-lg py-4 px-20 sticky top-0'>
+      <Link to='/' className="list-style-none text-decoration-none">
+        <header className='list-style-none text-decoration-none bg-gray-700 text-white font-semibold text-lg py-4 px-20 sticky top-0'>
           Amazon
         </header>
       </Link>
