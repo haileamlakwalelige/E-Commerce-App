723331213b1ab71d6b2c44fa0030c915496bc89e
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-10-14 15:48:46 +0300

    center the footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer =()=>{
     return (
-        <div>
+        <div className="text-center">
             <h1 className='bg-gray-700 text-white font-semibold text-lg py-4 px-20  bottom-0'>@2023 All Right is reserved to Haileamlak Waleligne</h1>
         </div>
     )
